72adeef6db258af72455a5935d6bb2d1782068c0
Kevin Luo <kwluo90@gmail.com>
2020-09-04 15:36:16 -0700

    Add stop verification

--- commands/dackchat/stop-mc.js
+++ commands/dackchat/stop-mc.js
@@ -6,7 +6,13 @@ AWS.config.update({region: 'us-west-2'});
 
 var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
-const stopmc = async (msg, fleetid) => {
+const stopmc = async (msg, fleetid, verifystop) => {
+  let channel = msg.channel;
+
+  if (verifystop.toLowerCase() != "yes") {
+      channel.send("HOLUP: Make sure you type /save-all and /stop in the minecraft server before running this command. If you have done so, retyp this command but add a 'yes' as the last argument.");
+      return;
+  }
 
   var params = {
       SplotFleetRequestIds: [
@@ -23,7 +29,7 @@ const stopmc = async (msg, fleetid) => {
   }
 
   // Send ack to channel that it is stopping
-  let channel = msg.channel;
+  
   channel.send('ACK, stopping server.');
 
 }
@@ -37,20 +43,27 @@ module.exports = class StopMcCommand extends commando.Command {
       group: 'dackchat',
       memberName: 'stopmc',
       description: 'Stops the DACK minecraft server',
-      examples: ['stopmc [spot_fleet_request_id]'],
+      examples: ['stopmc [spot_fleet_request_id] [yes if run /stop before this command]'],
       args: [
           {
               key: 'fleetId',
               label: 'fleetId',
               prompt: 'What\s the spot fleet request id?',
               type: 'string'
+          },
+          {
+              key: 'verifystop',
+              label: 'verifystop',
+              prompt: 'type \'yes\' if you verify you have run the /save-all and /stop command before running this command',
+              type: 'string',
+              default: 'no'
           }
       ]
     });
   }
 
-  async run(msg, {fleetid}) {
+  async run(msg, {fleetid, verifystop}) {
     console.log("Bot detected stopmc command from " + msg.author);
-    return await stopmc(msg, fleetid);
+    return await stopmc(msg, fleetid, verifystop);
   }
 }
